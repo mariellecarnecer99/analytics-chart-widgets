54222f46269e1c84fa63d87c73c36aacdcca656c
maynion <mcarnecer@startechup.com>
2023-09-08 14:14:41 +0800

    fixes

--- apps/chart-container/src/layouts/default/Default.vue
+++ apps/chart-container/src/layouts/default/Default.vue
@@ -1,12 +1,14 @@
 <template>
   <v-app>
     <default-bar />
-
-    <default-view />
   </v-app>
 </template>
 
-<script setup>
-  import DefaultBar from './AppBar.vue'
-  import DefaultView from './View.vue'
+<script>
+import DefaultBar from './AppBar.vue'
+export default {
+  components: {
+    DefaultBar
+  }
+}
 </script>
